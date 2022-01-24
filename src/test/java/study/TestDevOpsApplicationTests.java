c01034edb3155e035d24f96872d8e233416a2ec0
AmirKhan8435 <amirkhan141197@gmail.com>
2022-01-24 15:44:59 +0530

    Testing DevOp  Project

---
+++ src/test/java/study/TestDevOpsApplicationTests.java
@@ -0,0 +1,20 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() 
+	{
+		System.out.println("Hello World From Testing Through DevOps");
+	}
+
+}
+/*
+make directory git init where this project is created.
+make remote of git repository where u want to push 
+then add only test project 
+and then push project/remote */
